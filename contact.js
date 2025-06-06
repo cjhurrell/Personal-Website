eadf46c31a2922422a17b9230ed6b8f38a322047
Chris H <cjhurrell0@gmail.com>
2025-06-06 10:55:21 +0100

    connecting backend to front end server

--- contact.js
+++ contact.js
@@ -6,7 +6,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
   const message = document.getElementById('message').value.trim();
 
   try {
-    const res = await fetch('https://personal-website-production-752e.up.railway.app', {
+    const res = await fetch('https://personal-website-production-752e.up.railway.app/api/contact', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name, email, message })
